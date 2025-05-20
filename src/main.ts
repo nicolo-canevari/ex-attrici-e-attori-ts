e9dc43cd14b8c58b4e0fb37aa409603bd1bc0991
nicolo_canevari <nico.canevari@outlook.it>
2025-05-20 15:37:48 +0200

    Milestone 2

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+// MILESTONE 1
+
 // Definizione del tipo Person per rappresentare una persona generica
 type Person = {
 
@@ -9,3 +11,30 @@ type Person = {
   image: string;             // URL dell'immagine della persona
 
 };
+
+
+// MILESTONE 2
+
+// Insieme delle nazionalità accettate (union type)
+type Nationality =
+  | "American"
+  | "British"
+  | "Australian"
+  | "Israeli-American"
+  | "South African"
+  | "French"
+  | "Indian"
+  | "Israeli"
+  | "Spanish"
+  | "South Korean"
+  | "Chinese";
+
+// Tipo Actress che estende Person e aggiunge nuove proprietà
+type Actress = Person & {
+
+  most_famous_movies: [string, string, string]; // Tupla di 3 stringhe (array con un numero fisso di elementi, dove ogni elemento ha un tipo specifico e una posizione precisa)
+  awards: string;                               // Premi ricevuti (testo)
+  nationality: Nationality;                     // Solo valori definiti
+
+};
+
